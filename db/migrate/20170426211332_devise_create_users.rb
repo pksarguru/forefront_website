a73296cdc1b9a875d321c671d6a30a515a0dd7ea
Logan Price <logandsprice@gmail.com>
2017-04-26 18:54:33 -0500

    change username back to email

--- db/migrate/20170426211332_devise_create_users.rb
+++ db/migrate/20170426211332_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.boolean :admin, default: false
 
       ## Database authenticatable
-      t.string :username,           null: false, default: ""
+      t.string :email,           null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
