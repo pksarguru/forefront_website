4666598ae912b5672bf4ef9c77dcfa3431e94554
Logan Price <logandsprice@gmail.com>
2017-04-26 18:59:27 -0500

    Fix description field

--- db/migrate/20170426204509_create_projects.rb
+++ db/migrate/20170426204509_create_projects.rb
@@ -1,11 +1,11 @@
 class CreateProjects < ActiveRecord::Migration[5.0]
   def change
     create_table :projects do |t|
-      t.string      :team_name
-      t.string      :location
-      t.integer     :area
-      t.date        :completion_date
-      t.description :text
+      t.string  :team_name
+      t.string  :location
+      t.integer :area
+      t.date    :completion_date
+      t.text    :description
 
       t.timestamps
     end
