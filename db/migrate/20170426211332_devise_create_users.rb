5aa3052ea932834bafacb178a694498691bb3bc4
Logan Price <logandsprice@gmail.com>
2017-04-26 18:56:32 -0500

    Add first and last name fields to Users migration

--- db/migrate/20170426211332_devise_create_users.rb
+++ db/migrate/20170426211332_devise_create_users.rb
@@ -2,6 +2,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.boolean :admin, default: false
+      t.string  :first_name
+      t.string  :last_name
 
       ## Database authenticatable
       t.string :email,           null: false, default: ""
