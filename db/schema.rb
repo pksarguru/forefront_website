c7562948eb5d72f200fbed63443ec927c311760b
Pavan Sarguru <pavansarguru@gmail.com>
2017-05-02 15:08:43 -0500

    team index page and add bootstrap theme files into lib

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170428171122) do
     t.date     "completion_date"
     t.text     "description"
     t.boolean  "featured",        default: false
+    t.string   "category"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
   end
