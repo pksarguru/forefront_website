3f5abcdec96cb3ba46a7a2b720c528b1dade8e28
Pavan Sarguru <pavansarguru@gmail.com>
2017-05-05 19:40:16 -0500

    grab all objects in controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   def index
+    @featured_projects = Project.where(featured:"true").order(created_at: :desc)
+    @projects = Project.where(featured:"false").order(created_at: :desc)
   end
 
   def show
